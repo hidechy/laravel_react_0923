bf5833f68a0a8cd33f26472d9f06310da971cab1
toyohide <hide.toyoda@gmail.com>
2023-09-23 08:17:16 +0900

    20230923 seeder実行

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Task;
+
+class TaskFactory extends Factory
+{
+
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(rand(15, 40)),
+            'is_done' => $this->faker->boolean(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
